2cf5021f32e4f2031274f79dca0c2e513009978c
Vorapon Sirimahatham <vorapon_s@hotmail.com>
2021-03-13 18:15:46 +0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Vorapon Sirimahatham on 13/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
